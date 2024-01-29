435c12dfcde36d18961e51e20a2b4c1e73b3d796
markdennis <kirikamarkdennis@gmail.com>
2024-01-29 05:17:25 +0300

    my app

---
+++ front-end/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import {Container, Row, Col} from 'reactstrap';
+import GymmeNavbar from './components/navbar';
+
+const App = () => {
+  return (
+    <div className="App">
+      <GymmeNavbar></GymmeNavbar>
+      <Container>
+        <Row>
+          <Col md="2">
+          </Col>
+          <Col md="5">
+          </Col>
+          <Col md="3">
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/dev_run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# export environment variables to run server
+export FLASK_APP=server.py
+export FLASK_ENV=development
+
+# run the server
+flask run
